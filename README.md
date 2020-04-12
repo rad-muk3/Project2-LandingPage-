6b7c9767215eb6f394c15830d9ab170817be40f7
Radhika <amukund6@gatech.edu>
2020-04-12 14:24:12 -0700

    Final Version of Landing-Page Project

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# Project2-LandingPage-
-This project uses Javascript DOM to add menus and scrolls to sections in the viewport and highlights menu-links for active pages
+# Landing Page Project
+
+## Table of Contents
+
+* [Instructions](#instructions)
+
+## Instructions
+
+This project uses the starter files of the static Landing-Project provided. The following static files have been modified to make the Landing-Page project as dynamic.
+index.hml - this file has been modified to inlcude classes for active section and collapsing-sections for readability.
+styles.css - has been modified to style the active menu links and sections and also collapsible sections 
+app.js - this file is driving file , being called from within the body of the index.html. The following functionality has been added to
+make the Landing-Page dynamic.
+1. Navigation is built dynamically as an unordered list.
+2. Shows which  section is being viewed while scrolling through the page and highlights the active menu item.
+3. When clicking an item from the navigation menu, the link scrolls to the appropriate section.
+4. To view the contents of Each Section, a collapsible functionality has been added to each section. If you click on the plus or
+minus icon the contents show or hide appropriately. This helps with readability.
+5. A Top button is available while scrolling and when clicking on Top button takes you to the top of the Page.
+To view this Dynamic Landing Page, open index.html on any browser and test the above points.
--- js/app.js
+++ js/app.js
@@ -20,6 +20,7 @@
 
 const sectionItemElements = document.querySelectorAll('[data-nav]');
 const sectionTags = document.getElementsByTagName("section");
+const divElem = document.getElementById("topBtn");
 
 /**
  * End Global Variables
@@ -27,26 +28,37 @@ const sectionTags = document.getElementsByTagName("section");
  *
  */
 
-
-//Helper function to find out which section is being viewed by the user at a given moment.
-//Got this idea from an article online: https://gomakethings.com/how-to-test-if-an-element-is-in-the-viewport-with-vanilla-javascript/
+/**
+ * @description sectionInViewport
+ * @description Helper function to find out which section is being viewed by the user at a given moment.
+ * @description Got this idea from an article online: https://gomakethings.com/how-to-test-if-an-element-is-in-the-viewport-with-vanilla-javascript/
+ * @param {elem} sectionId
+ * @returns {vRect} the viewport of the section element
+ */
 const sectionInViewport = (elem) => {
-  const vRect = elem.getBoundingClientRect();
 
+  const vRect = elem.getBoundingClientRect();
   return (
     vRect.top >= 0 &&
     vRect.left >= 0 &&
     vRect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
+    (window.innerHeight || document.documentElement.clientHeight) &&
     vRect.right <=
     (window.innerWidth || document.documentElement.clientWidth)
   );
 };
 
-//Helper function to make a menu item link active
+/**
+ * @description makeLinkActive
+ * @description Helper function to make a menu item link active for the current-section on viewport
+ * @param {secId} sectionId
+ */
 const makeLinkActive = (secId) => {
+
   const navLink = document.querySelectorAll('[data-scroll]');
+
   for (const link of navLink) {
+
     if (secId === link.getAttribute('data-scroll')) {
       link.classList.add('active');
     } else {
@@ -56,79 +68,163 @@ const makeLinkActive = (secId) => {
 };
 
 /**
+ * @description scrollToViewBtn
+ * @description When the user scrolls down 20px from the top of the document, show the button
+ *
+ */
+const scrollToViewBtn = () => {
+  window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      divElem.style.display = "block";
+    } else {
+      divElem.style.display = "none";
+    }
+
+
+  };
+};
+
+/**
+ * @description scrolltoTop
+ * @description When the user clicks the button scroll to the top of the page
+ *
+ */
+const scrolltoTop = () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+
+};
+
+/**
  * End Helper Functions
  * Begin Main Functions
  *
  */
 
-// build the nav
+/**
+ * @description Add Menu Items to the Navigation Bar
+ *
+ */
 
 const addMenus = () => {
+
   //Create li (menu-list items) and append it to the Ul (navabar-list)
   const ulContainer = document.getElementById('navbar__list');
 
   const liItem = document.createElement('li');
-  //Set the text of the Menu List Items
-  //TODO: Try to write an array for new Menu text items
-  //liItem.innerHTML = '<a class="menu__link">Home</a>';
-  //navMenu.appendChild(home);
 
+  // TODO: Write an array for new Menu Text Item to be appended to the Navbar
 
-  for(let elem of sectionTags){
-    //create a list item
-    let menuLi = document.createElement("li");
+  for (let elem of sectionTags) {
+
     //get the dat-nav attribute for the menu text name
     const menuTextName = elem.getAttribute("data-nav");
-
-    const navItemLink = elem.getAttribute("id");
-    menuLi.className = "menu__link";
+    const navItemLink = elem.getAttribute('id');
+    //create a list item
+    let menuLi = document.createElement('li');
+    menuLi.className = 'menu__link';
     //using templates, set the liItem attributes like ahref+scrollId+data-nav
     menuLi.innerHTML = `<a href="#${elem.id}" data-scroll="${navItemLink}">${menuTextName}</a>`;
-
     //add menu item to the ul menubar
     ulContainer.appendChild(menuLi);
   }
 };
 
-//Scroll when clicking a menu items
+/**
+ * @description Scroll to the corresponding section on top of viewport when clicking a menu items
+ *
+ */
 const scrollToEachSection = () => {
-    let anchorlinks = document.querySelectorAll('a[href^="#"]');
 
-    for (let item of anchorlinks) {
-        item.addEventListener('click', (e)=> {
-            let attrVal = item.getAttribute('href');
-            let target = document.querySelector(attrVal);
-            target.scrollIntoView({
-                behavior: 'smooth'
-            })
-            history.pushState(null, null, attrVal);
-            e.preventDefault();
-        })
-    }
+  let anchorlinks = document.querySelectorAll('a[href^="#"]');
+
+  for (let item of anchorlinks) {
+
+    item.addEventListener('click', (e) => {
+
+      let attrVal = item.getAttribute('href');
+      let target = document.querySelector(attrVal);
+      target.scrollIntoView({
+        behavior: 'smooth'
+      })
+      history.pushState(null, null, attrVal);
+      e.preventDefault();
+    })
+  }
 }
 
-// Add class 'active' to section when near top of viewport
-
-// This section code involves 3 steps and calls Helper Functions
-//STEP 1: On Scroll event in the browser, iterate through every section
-// and check if it is in the view-port. InViewport uses (getBoundingClientRect()) tp achieve this .
-//STEP 2: If section on viewport, add CSS-active-section class to the current section
-//and also add CSS-active-link class to the current link.else remove those active CSS classes
-//STEP 3: Write CSS selector for active-link and active-section to make them appear accordingly.
+/**
+ * @description Add class 'active' to section when near top of viewport
+ * @description This section code involves 3 steps and calls Helper Functions
+ * @description STEP 1: On Scroll event in the browser, iterate through every section.and check if it is in the view-port. InViewport uses (getBoundingClientRect()) tp achieve this .
+ * @description STEP 2: If section on viewport, add CSS-active-section class to the current section and also add CSS-active-link class to the current link.else remove those active CSS classes
+ * @description STEP 3:  Write CSS selector for active-link and active-section to make them appear accordingly.
+ */
 
 const makeActive = () => {
-  //window.onscroll = function () {
-    for (const elem of sectionItemElements) {
-      if (sectionInViewport(elem)) {
-        elem.classList.add('section-current');
-        makeLinkActive(elem.getAttribute('id'));
-      } else {
-        elem.classList.remove('section-current');
-      }
+
+  for (const elem of sectionItemElements) {
+
+    if (sectionInViewport(elem)) {
+
+      elem.classList.add('section-current');
+      makeLinkActive(elem.getAttribute('id'));
+
+    } else {
+
+      elem.classList.remove('section-current');
     }
-  //};
+  }
+
+
 };
 
+/**
+ * @description collapseSection
+ * @description Makes Section Collapsible
+ *
+ */
+const collapseSection = () => {
+  const coll = document.getElementsByClassName("collapsible");
+
+  for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("current");
+      let content = this.nextElementSibling;
+      if (content.style.display === "none") {
+        content.style.display = "block";
+      } else {
+        content.style.display = "none";
+      }
+    });
+  }
+};
+
+/**
+ * @description showTopBtn
+ * @description Create a Button and set its text.
+ * @description Append this button the div element inside the Footer Section.
+ * @description Style the button and position it on the right corner of the Footer.
+ * @description Used W3Schools Tutorial for this functionality
+ */
+const showTopBtn = () => {
+
+  const btn = document.createElement('BUTTON');
+  const btnText = document.createTextNode("TOP");
+
+  btn.appendChild(btnText);
+  divElem.appendChild(btn);
+
+  // Scroll from top down to view the button
+  scrollToViewBtn();
+
+  // When the user clicks on the button, scroll to the top of the document
+  btn.onclick = () => {
+    scrolltoTop();
+
+  }
+
+};
 
 /**
  * End Main Functions
@@ -145,5 +241,13 @@ scrollToEachSection();
 
 // Set sections and menu links as Active on scrolling
 document.addEventListener("scroll", function() {
+
   makeActive();
+
 });
+
+//Collapse Each Section
+collapseSection();
+
+//Add a scrollTotop button on the page
+showTopBtn();
